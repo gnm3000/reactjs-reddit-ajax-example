7c78ce6927799e8b75ee137bd85d24a6939c5753
German <gnm3000@gmail.com>
2020-03-12 20:41:36 -0300

    OK

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+function Reddit() {
+    const [posts, setPosts] = React.useState([]);
+    React.useEffect(() => {
+        axios.get(`https://www.reddit.com/r/reactjs.json`)
+          .then(res => {
+            const newPosts = res.data.data.children
+              .map(obj => obj.data);
+      
+            setPosts(newPosts);
+          });
+      }, []);
+    return (
+      <div>
+        <h1>/r/reactjs</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}<br/>{post.author_fullname}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+ReactDOM.render(<Reddit />, document.getElementById("root"));
